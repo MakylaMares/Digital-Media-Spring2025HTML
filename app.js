db6c531b866835790e0c8e3c833a8044754878bb
MakylaMares <Makylam03@gmail.com>
2025-01-26 05:24:01 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('Hey Dev Ed') ;
+var life = 100; // This is my Life bar  (NUMBER)
+
+life = life - 10;
+
+//90
+
+life = life / 10;
+
+var name = 'Dev Ed'; // This is the name display in the game  (STRING)
+
+var checkout = false; // (BOOLEAN)
+
+var box; // This is my box varible 
+
+var global = 'Hello' // This is my global varible
+
+const myObject = {
+    property1: 'value1'  // This is my const Object
+};
+
+//OBJECT SYNBOL
+
+console.log('Hey Dev Ed!');
+
+console.log(life);
