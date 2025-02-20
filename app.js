ad7a529eb6899d7aacca0f0240425fa519370a5c
MakylaMares <Makylam03@gmail.com>
2025-02-20 00:43:44 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,68 @@
+const names = ["Makyla", "Jacky", "Xavier", "Matt", "Christian"];
+
+
+for(name of names ){
+    console.log('Hello there ${name}');
+}
+///Loop For
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+  }
+///Loop For
+
+let n = 0;
+
+while (n < 3) {
+  n++;
+}
+
+console.log(n);
+///Loop While
+
+const userlist = document.querySelectorAll(".name-list li");
+const listInput = document.querySelector(".list-input");
+const addListBtn = document.querySelector(".addListBtn");
+
+addListBtn.addEventListener('click', function(){
+    const newLi = document.createElement('LI');
+    const liContent = document.createTextNode(listInput.value);
+    newLi.appendChild(liContent);
+    userlist,appendChild(newLi);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const user = {
     name: 'Makyla',
     age: 22,
