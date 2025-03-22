3dc3085ce99fdb7e160adcf40484ddfdd1640937
MakylaMares <Makylam03@gmail.com>
2025-03-21 20:36:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,46 +1,116 @@
-const names = ["Kayla", "Juan", "Josh", "Jess", "Fred", "lucky"];
+const game = () => {
+  let pScore = 0;
+  let cScore = 0;
 
-for(name of names){
-    console.log(name);
-}
+  //Start the Game
+  const startGame = () => {
+    const playBtn = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
 
-let n = 0;
+    playBtn.addEventListener("click", () => {
+      introScreen.classList.add("fadeOut");
+      match.classList.add("fadeIn");
+    });
+  };
+  //Play Match
+  const playMatch = () => {
+    const options = document.querySelectorAll(".options button");
+    const playerHand = document.querySelector(".player-hand");
+    const computerHand = document.querySelector(".computer-hand");
+    const hands = document.querySelectorAll(".hands img");
 
-while (n < 3) {
-  n++;
-}
+    hands.forEach(hand => {
+      hand.addEventListener("animationend", function() {
+        this.style.animation = "";
+      });
+    });
+    //Computer Options
+    const computerOptions = ["rock", "paper", "scissors"];
 
-console.log(n);
+    options.forEach(option => {
+      option.addEventListener("click", function() {
+        //Computer Choice
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber];
 
-const newH1 = document.createElement("h1");
-newH1.id = "myH1";
-newH1.style.color = "tomato";
-newH1.style.textAlign = "center";
+        setTimeout(() => {
+          //Here is where we call compare hands
+          compareHands(this.textContent, computerChoice);
+          //Update Images
+          playerHand.src = `./assets/${this.textContent}.png`;
+          computerHand.src = `./assets/${computerChoice}.png`;
+        }, 2000);
+        //Animation
+        playerHand.style.animation = "shakePlayer 2s ease";
+        computerHand.style.animation = "shakeComputer 2s ease";
+      });
+    });
+  };
 
-newH1.textContent = "I like Pizza!";
+  const updateScore = () => {
+    const playerScore = document.querySelector(".player-score p");
+    const computerScore = document.querySelector(".computer-score p");
+    playerScore.textContent = pScore;
+    computerScore.textContent = cScore;
+  };
 
-const box2 = document.getElementById("box2");
-document.body.insertBefore(newH1, box2);
+  const compareHands = (playerChoice, computerChoice) => {
+    //Update Text
+    const winner = document.querySelector(".winner");
+    //Checking for a tie
+    if (playerChoice === computerChoice) {
+      winner.textContent = "It is a tie";
+      return;
+    }
+    //Check for Rock
+    if (playerChoice === "rock") {
+      if (computerChoice === "scissors") {
+        winner.textContent = "Player Wins";
+        pScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Computer Wins";
+        cScore++;
+        updateScore();
+        return;
+      }
+    }
+    //Check for Paper
+    if (playerChoice === "paper") {
+      if (computerChoice === "scissors") {
+        winner.textContent = "Computer Wins";
+        cScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Player Wins";
+        pScore++;
+        updateScore();
+        return;
+      }
+    }
+    //Check for Scissors
+    if (playerChoice === "scissors") {
+      if (computerChoice === "rock") {
+        winner.textContent = "Computer Wins";
+        cScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Player Wins";
+        pScore++;
+        updateScore();
+        return;
+      }
+    }
+  };
 
+  //Is call all the inner function
+  startGame();
+  playMatch();
+};
 
-let fruits = ["apple", "orange", "banana"];
-
-fruits.unshift("mango");
-
-console.log(fruits[0]);
-console.log(fruits[1]);
-console.log(fruits[2]);
-console.log(fruits[3]);
-
-const person1 = {
-    firstName: "Makyla",
-    lastName: "Mares",
-    age: 30,
-    isEmployed: true,
-    sayHello: function(){console.log("Hi! I am Makyla!")},
-    eat: function(){console.log("I am eating a Mango")},
-}
-
-console.log(person1.firstName);
-person1.sayHello();
-person1.eat();
+//start the game function
+game();
